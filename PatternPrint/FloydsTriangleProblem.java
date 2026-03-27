4ef7677464eb3a5f7afbd528e00d3c204aa59df3
Ankit275di <145134888+Ankit275di@users.noreply.github.com>
2026-03-27 21:38:31 +0530

    New Update Save

---
+++ PatternPrint/FloydsTriangleProblem.java
@@ -0,0 +1,14 @@
+public class FloydsTriangleProblem {
+    public static void main(String[] args) {
+        int n= 5;
+        int number=1;
+        // outer loop for rows
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }    
+}
