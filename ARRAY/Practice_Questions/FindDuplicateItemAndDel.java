ed1858457995029637792c17057147eb1a1f00ac
Ankit275di <145134888+Ankit275di@users.noreply.github.com>
2026-04-17 04:37:32 +0530

    edit

---
+++ ARRAY/Practice_Questions/FindDuplicateItemAndDel.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class FindDuplicateItemAndDel{
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the limit: ");
+        int n = sc.nextInt();
+        
+        int arr[] = new int[n];
+        
+        System.out.println("Enter the Array Elements: ");
+        for(int i=0; i<=n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        System.out.println("Displaying the items: ");
+        for(int i=0; i<=n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
