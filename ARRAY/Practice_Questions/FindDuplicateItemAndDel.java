aacf751aa79ca94bb5fd3100b7708ee8f5083320
Ankit275di <145134888+Ankit275di@users.noreply.github.com>
2026-04-17 04:45:00 +0530

    edit

--- ARRAY/Practice_Questions/FindDuplicateItemAndDel.java
+++ ARRAY/Practice_Questions/FindDuplicateItemAndDel.java
@@ -10,13 +10,15 @@ public class FindDuplicateItemAndDel{
         int arr[] = new int[n];
         
         System.out.println("Enter the Array Elements: ");
-        for(int i=0; i<=n; i++){
+        for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
         
         System.out.println("Displaying the items: ");
-        for(int i=0; i<=n; i++){
+        for(int i=0; i<n; i++){
             System.out.print(arr[i]+" ");
         }
+
+
     }
 }
